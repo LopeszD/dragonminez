ad362c1faccca3bd0fba9a3b768421a9cd21046a
Sleep <83037839+KyoSleep1@users.noreply.github.com>
2025-05-08 00:24:48 -0700

    feat: added stat's constant values to Reference

--- src/main/java/com/dragonminez/mod/common/Reference.java
+++ src/main/java/com/dragonminez/mod/common/Reference.java
@@ -1,5 +1,7 @@
 package com.dragonminez.mod.common;
 
+import net.minecraft.resources.ResourceLocation;
+
 /**
  * The {@code Reference} class stores static constants used throughout the mod.
  * <p>
@@ -21,6 +23,41 @@ public final class Reference {
      */
     public static final String MOD_ID = "dragonminez";
 
+    /**
+     * The version of the mod.
+     * <p>
+     * This version is used for tracking updates and compatibility with different
+     * versions of Minecraft and Forge.
+     * </p>
+     */
+    public static final String VERSION = "2.0.0";
+
+    /**
+     * The empty string used to represent an uninitialized or default state.
+     * <p>
+     * This constant is used in various parts of the mod to indicate that a
+     * particular value has not been set or is not applicable.
+     * </p>
+     */
+    public static final String EMPTY = "%s:empty".formatted(MOD_ID);
+
+
     // Private constructor to prevent instantiation
-    private Reference() {}
+    private Reference() {
+    }
+
+    public static class Stat {
+
+        public static final ResourceLocation CAP_ID = new ResourceLocation("dragonminez:stat");
+        public static final String RACE = "race";
+        public static final String FORM = "form";
+        public static final String STRENGTH = "strength";
+        public static final String DEXTERITY = "dexterity";
+        public static final String CONSTITUTION = "constitution";
+        public static final String ENERGY = "energy";
+        public static final String POWER = "power";
+        public static final String ALIGNMENT = "alignment";
+        public static final String COMBAT_MODE = "isInCombatMode";
+
+    }
 }
